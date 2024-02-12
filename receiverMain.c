8ee327527c65993efbd5bd1ada2225856fc1f3ec
Inomaty <tamagogyunyu@gmail.com>
2024-02-12 19:36:49 +0900

    add MPLAB configration files

---
+++ receiverMain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   receiverMain.c
+ * Author: tamag
+ *
+ * Created on 2024/02/12, 18:59
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
